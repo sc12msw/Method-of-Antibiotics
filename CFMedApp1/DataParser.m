4f38b965c1abe078e77f14a52870c21a0d923df0
Matt Watson <BeacHBuM614@Matts-Mac-mini.local>
2015-02-23 18:24:12 +0000

    Added some processing for the xml file. Edited heights for widgets on drug detail to adapt for iPad.

--- CFMedApp1/DataParser.m
+++ CFMedApp1/DataParser.m
@@ -26,7 +26,44 @@
     
     NSLog(@"%@", doc.rootElement);
     
+    int currentIndex =0;
+    NSString *drugName, *type, *sideEffect,*interaction;
     
+    NSArray *drugs = [doc.rootElement elementsForName:@"drug"];
+    for (GDataXMLElement *obj in drugs){
+        NSArray *drugNames = [obj elementsForName:@"name"];
+        if (drugs.count > 0) {
+            GDataXMLElement *drugXML = (GDataXMLElement *) [drugNames objectAtIndex:currentIndex];
+            drugName = drugXML.stringValue;
+        }else continue;
+        
+        NSArray *types =[obj elementsForName:@"type"];
+        if (types.count >0){
+            GDataXMLElement *typeXML = (GDataXMLElement *) [types objectAtIndex:currentIndex];
+            type = typeXML.stringValue;
+        }else continue;
+        
+        NSArray *sideEffects =[obj elementsForName:@"side_effects"];
+        if (sideEffects.count >0){
+            GDataXMLElement *sideEffectXML = (GDataXMLElement *) [sideEffects objectAtIndex:currentIndex];
+            sideEffect = sideEffectXML.stringValue;
+        }else continue;
+        
+        NSArray *interactions =[obj elementsForName:@"warning"];
+        if (interactions.count >0){
+            GDataXMLElement *interactionXML = (GDataXMLElement *) [interactions objectAtIndex:currentIndex];
+            interaction = interactionXML.stringValue;
+        }else continue;
+        
+        NSArray *treatmentsFor =[obj elementsForName:@"treatment"];
+        if (treatmentsFor.count >0){
+            GDataXMLElement *treatmentXML = (GDataXMLElement *) [treatmentsFor objectAtIndex:currentIndex];
+            interaction = treatmentXML.stringValue;
+        }else continue;
+
+        
+        currentIndex = currentIndex +1;
+    }
     
     return nil;
     
