b7c614ae768fd84a4994ed4dcf155df060b868cb
sc12msw <beachbum614@hotmail.com>
2015-03-15 18:51:40 +0000

    created admin interface

--- Server/com.watson.jersey.cfmed/src/com/watson/jersey/cfmed/AdultInfo.java
+++ Server/com.watson.jersey.cfmed/src/com/watson/jersey/cfmed/AdultInfo.java
@@ -2,7 +2,39 @@ package com.watson.jersey.cfmed;
 //XML element classes.
 public class AdultInfo {
 
-	private AdminDose adminDoseOral,adminDoseInhaled;
+	private AdminDose adminDoseOral,adminDoseInhaled,adminDosePR,adminDoseSC,adminDoseIM,adminDoseIV;
+
+	public AdminDose getPR() {
+		return adminDosePR;
+	}
+
+	public void setPR(AdminDose adminDosePR) {
+		this.adminDosePR = adminDosePR;
+	}
+
+	public AdminDose getSC() {
+		return adminDoseSC;
+	}
+
+	public void setSC(AdminDose adminDoseSC) {
+		this.adminDoseSC = adminDoseSC;
+	}
+
+	public AdminDose getIM() {
+		return adminDoseIM;
+	}
+
+	public void setIM(AdminDose adminDoseIM) {
+		this.adminDoseIM = adminDoseIM;
+	}
+
+	public AdminDose getIV() {
+		return adminDoseIV;
+	}
+
+	public void setIV(AdminDose adminDoseIV) {
+		this.adminDoseIV = adminDoseIV;
+	}
 
 	public AdminDose getOral() {
 		return adminDoseOral;
--- Server/com.watson.jersey.cfmed/src/com/watson/jersey/cfmed/RestLogic.java
+++ Server/com.watson.jersey.cfmed/src/com/watson/jersey/cfmed/RestLogic.java
@@ -1,5 +1,6 @@
 package com.watson.jersey.cfmed;
 
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -17,7 +18,6 @@ import javax.xml.bind.*;
 
 @Path("/cfmed")
 public class RestLogic {
-
 
 	public CFInfo createObj(){
 		
@@ -40,7 +40,9 @@ public class RestLogic {
 		drugInfo.setIndication("For this disease");
 		drugInfo.setInteractions("This drug interacts with : 1drug 2drug etc");
 		drugInfo.setSideEffect("This is a side effect");
-		String[] brands = {"brand 1","brand 2"};
+		ArrayList <String> brands = new ArrayList <String>();
+		brands.add("brand 1");
+		brands.add("brand 2");
 		drugInfo.setBrandName(brands);
 		
 		pathoInfo.setName("Bacteria");
@@ -69,16 +71,14 @@ public class RestLogic {
 		cfInfo.setPathogen(pathoArray);
 		return cfInfo;
 	}
-
+	
 
 	// This method returns the xml of the objects created on the server. Using JAXB
 	@GET
 	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
 	public File getXML() throws IOException {
-		File file4 = new File("highscore.txt");
-		System.out.println(file4.getCanonicalPath());
-		File file = new File("file.xml");
-		CFInfo cfInfo = createObj();
+		File file = new File("cfInfo.xml");
+		/*
 		try{
 
 			JAXBContext jaxbContext = JAXBContext.newInstance(CFInfo.class);
@@ -91,12 +91,13 @@ public class RestLogic {
 		}catch (JAXBException e) {
 			e.printStackTrace();
 		}
+		*/
 		return file;
 	}
 
 //This method returns html instead of xml to view in web browser. Using the path rest/cfmed/html
 	@GET
-	@Path("html")
+	@Path("/html")
 	@Produces(MediaType.TEXT_HTML)
 	public File getHTML()  {
 		//Gets the template for the HTML.
@@ -124,7 +125,7 @@ public class RestLogic {
 		
 		//Fills the template with data.
 		CFInfo cfInfo = createObj();
-		String[] tempArray = null;
+		ArrayList <String> tempArray = new ArrayList<String>();
 		String tempString = "";
 		String row = "";
 		String title = "Cystic Fybrosis Information";
@@ -136,8 +137,8 @@ public class RestLogic {
 		PaediatricInfo pInfo = drugInfo.getPaediatric();
 		AdminDose pOral = pInfo.getOral();
 		tempArray = drugInfo.getBrandName();
-		for (int j=0; j<tempArray.length; j++){
-			tempString = tempString +" "+ tempArray[j];
+		for (int j=0; j<tempArray.size(); j++){
+			tempString = tempString +" "+ tempArray.indexOf(j);
 		}
 		//Puts all drugs into a table
 		row =row + "<tr> <td>"+drugInfo.getName()+"</td>"+"<td>"+tempString+"</td>"+"<td>"+drugInfo.getIndication()+"</td>"+"<td>"+adultOral.getAdministration()+"</td>"+"<td>"+adultOral.getDose()+"</td>"+"<td>"+pOral.getAdministration()+"</td>"+"<td>"+pOral.getDose()+"</td>"+"<td>"+drugInfo.getInteractions()+"</td>";
@@ -160,4 +161,9 @@ public class RestLogic {
 		    
 		return file;
 	}
+	
+	
+	
+	
+	
 }
