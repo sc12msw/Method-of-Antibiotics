939795fa852a1e96e5ea486445f418f5d90fc8bf
sc12msw <beachbum614@hotmail.com>
2015-03-15 19:31:37 +0000

    Readded the server
    Couldn't get around this issue So i readded the server.

---
+++ Server/com.watson.jersey.cfmed/src/com/watson/jersey/cfmed/AdminDose.java
@@ -0,0 +1,18 @@
+package com.watson.jersey.cfmed;
+//XML element classes.
+public class AdminDose {
+private String administration;
+private String dose;
+public String getAdministration() {
+	return administration;
+}
+public void setAdministration(String administration) {
+	this.administration = administration;
+}
+public String getDose() {
+	return dose;
+}
+public void setDose(String dose) {
+	this.dose = dose;
+}
+}
